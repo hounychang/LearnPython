d90bab60bf5f5423a7fee57ece8cd44acba113c1
Houny Chang <hounychang@outlook.com>
2015-04-28 15:25:12 +0800

    setup the environment and print the 'Hello World'

---
+++ Base/D_00_HelloWorld.py
@@ -0,0 +1,7 @@
+__author__ = 'James.Hongnian.Zhang'
+# This is my first Python program.
+# This means I have setup the environment for Python.
+# Download it from https://www.python.org and install it.
+# Then add it to your PATH
+# That's it.
+print 'Hello World'
